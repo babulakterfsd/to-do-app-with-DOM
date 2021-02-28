5f286c473cfdd311ca6dd249f5484e712cbad27f
babulakterfsd <babulakterfsd@gmail.com>
2021-02-28 19:34:12 +0600

    major changes

--- js/main.js
+++ js/main.js
@@ -40,35 +40,107 @@ form.addEventListener(`submit`, function(e) {
     this.reset()
 }) */
 
-let text;
-let inputvalue = document.getElementById(`task`);
-inputvalue.addEventListener(`change`, function(e) {
-        text = e.target.value
+// let text;
+// let inputvalue = document.getElementById(`task`);
+// inputvalue.addEventListener(`change`, function(e) {
+//         text = e.target.value
+// })
+
+// let form = document.getElementById(`form`);
+// form.addEventListener(`submit`,function(e) {
+//     e.preventDefault();
+//     createFormCheck();
+//     this.reset();
+// })
+
+// function createFormCheck() {
+//     let formCheckDiv = document.createElement(`div`);
+//     formCheckDiv.className = `form-check`;
+
+//     let checkBox = document.createElement(`input`);
+//     checkBox.type = `checkbox`;
+//     checkBox.className = `form-check-input`;
+
+//     let label = document.createElement(`label`);
+//     label.className = `form-check-label`;
+//     label.innerText = text;
+
+//     formCheckDiv.appendChild(checkBox);
+//     formCheckDiv.appendChild(label);
+
+//     let parent = document.getElementById(`incomplete-task`);
+//     parent.appendChild(formCheckDiv)
+// }
+
+
+
+
+
+
+let allTask = [];
+let completedTask = [];
+let task;
+
+
+document.getElementById('task').addEventListener('keyup', e => {
+    task = e.target.value.trim();
 })
 
-let form = document.getElementById(`form`);
-form.addEventListener(`submit`,function(e) {
+
+document.getElementById('form').addEventListener('submit', e=>{
     e.preventDefault();
-    createFormCheck();
-    this.reset();
+    createIncompleteTaskList(task);
+    allTask.push(task);
+    e.target.reset();
 })
 
-function createFormCheck() {
-    let formCheckDiv = document.createElement(`div`);
-    formCheckDiv.className = `form-check`;
 
-    let checkBox = document.createElement(`input`);
-    checkBox.type = `checkbox`;
-    checkBox.className = `form-check-input`;
+function createIncompleteTaskList(task){
+    const ul = document.getElementById(`incomplete-task`);
+    ul.style.listStyleType = 'none';
+    ul.innerHTML += `<li><input style="margin-right:5px;" type='checkbox' />${task}</li>`;
+}
 
-    let label = document.createElement(`label`);
-    label.className = `form-check-label`;
-    label.innerText = text;
 
-    formCheckDiv.appendChild(checkBox);
-    formCheckDiv.appendChild(label);
 
-    let parent = document.getElementById(`incomplete-task`);
-    parent.appendChild(formCheckDiv)
+
+const ul = document.getElementById(`incomplete-task`);
+ul.addEventListener('click', e => {
+    if(e.target.type === 'checkbox'){
+        completedTask.push(e.target.parentElement.innerText.trim());
+    }
+})
+
+
+
+
+
+function submitCompletedTask() {
+    console.log(completedTask)
+    console.log(allTask)
+    if(completedTask.length !== 0){
+        document.getElementById(`incomplete-task`).innerHTML = '';
+        
+        const filteredTask = allTask.filter(alt => !completedTask.includes(alt));
+
+        console.log(filteredTask);
+
+        filteredTask.forEach(v => {
+            createIncompleteTaskList(v);
+        });
+        setCompletedTask(completedTask);
+        allTask = filteredTask;
+        completedTask = [];
+    }
+
+}
+
+
+function setCompletedTask(task){
+    const ul = document.getElementById("completed-task-list");
+    ul.style.listStyleType = "none";
+    task.forEach(v => {
+        ul.innerHTML+= `<li>${v}</li>`
+    });
 }
 
